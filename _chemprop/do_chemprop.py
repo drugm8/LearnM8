821a840714a4571164212cf8d6165a757b8c56f8
Max <maba00001@stud.uni-saarland.de>
2024-09-23 11:12:03 +0200

    results: score improvements with just predicting Energy functions

--- _chemprop/do_chemprop.py
+++ _chemprop/do_chemprop.py
@@ -1,64 +1,69 @@
 import pandas as pd
 from pathlib import Path
+
 from lightning import pytorch as pl
+
 from chemprop import data, featurizers, models, nn
-from sklearn.model_selection import train_test_split
 
+def do_chempop(smiles, ys ):
+    #!careful target columns was a list of strings
+    yss=ys.reshape(-1, 1)
 
-#IMPROVING CHEMPROP TRIAL
-def do_chempop(smiles, ys):
-    yss = ys.reshape(-1, 1)
-    num_workers = 8
+    num_workers = 8 # number of workers for dataloader. 0 means using main process for data loading
+    
+    
+
 
     all_data = [data.MoleculeDatapoint.from_smi(smi, y) for smi, y in zip(smiles, yss)]
+    #!some fucky object floating around that is my new datapoint
+    #TODO literature research what the f im inputting
 
-    # Split data into train and validation sets
-    train_data, val_data = train_test_split(all_data, test_size=0.2, random_state=42)
+    list(data.SplitType.keys())
 
-    # Use a more advanced featurizer
-    featurizer = featurizers.MolGraphFeaturizer(include_charge=True, include_chirality=True)
+    train_data = all_data
+
+    featurizer = featurizers.SimpleMoleculeMolGraphFeaturizer()
 
     train_dset = data.MoleculeDataset(train_data, featurizer)
     scaler = train_dset.normalize_targets()
 
-    val_dset = data.MoleculeDataset(val_data, featurizer)
-    val_dset.normalize_targets(scaler)
+    # val_dset = data.MoleculeDataset(val_data, featurizer)
+    # val_dset.normalize_targets(scaler)
 
-    train_loader = data.build_dataloader(train_dset, num_workers=num_workers, batch_size=32)
-    val_loader = data.build_dataloader(val_dset, num_workers=num_workers, batch_size=32)
 
-    # Use more complex message passing and aggregation
-    mp = nn.BondMessagePassing(hidden_size=300, depth=5)
-    agg = nn.AttentionAggregation()
+    train_loader = data.build_dataloader(train_dset, num_workers=num_workers)
+
+    mp = nn.BondMessagePassing()
+
+
+    print(nn.agg.AggregationRegistry)
+
+
+    agg = nn.MeanAggregation()
+    print(nn.PredictorRegistry)
 
     output_transform = nn.UnscaleTransform.from_standard_scaler(scaler)
 
-    # Use a more complex FFN
-    ffn = nn.RegressionFFN(hidden_size=300, num_layers=3, dropout=0.1, output_transform=output_transform)
+    ffn = nn.RegressionFFN(output_transform=output_transform)
 
     batch_norm = True
 
-    metric_list = [nn.metrics.RMSEMetric(), nn.metrics.MAEMetric(), nn.metrics.R2Metric()]
+
+    print(nn.metrics.MetricRegistry)
+
+    metric_list = [nn.metrics.RMSEMetric(), nn.metrics.MAEMetric()] # Only the first metric is used for training and early stopping
 
     mpnn = models.MPNN(mp, agg, ffn, batch_norm, metric_list)
 
-    # Learning rate scheduler
-    lr_scheduler = pl.optimizers.lr_scheduler.ReduceLROnPlateau(
-        mode='min', factor=0.1, patience=5, min_lr=1e-6
-    )
-
     trainer = pl.Trainer(
-        logger=True,
-        enable_checkpointing=True,
+        logger=False,
+        enable_checkpointing=True, # Use `True` if you want to save model checkpoints. The checkpoints will be saved in the `checkpoints` folder.
         enable_progress_bar=True,
         accelerator="auto",
-        max_epochs=100,  # Increased number of epochs
-        callbacks=[
-            pl.callbacks.EarlyStopping(monitor='val_loss', patience=10),
-            pl.callbacks.LearningRateMonitor(logging_interval='epoch'),
-        ],
+        max_epochs=20, # number of epochs to train for
     )
 
-    trainer.fit(mpnn, train_loader, val_loader)
+    trainer.fit(mpnn, train_loader)
 
     return trainer, mpnn
+
--- script_templates/rf_hyperparameters_on_WSAV.py
+++ script_templates/rf_hyperparameters_on_WSAV.py
@@ -6,21 +6,34 @@ from helpers.query_functions import greedy_query_function, random_query_function
 from helpers.scoring_metric import top_x_of_x_percentage
 from learners.rf_learner import rf_learner
 from learners.gp_learner import gp_learner
+from rdkit import Chem
+from rdkit.Chem import rdFingerprintGenerator
 from sklearn.ensemble import RandomForestRegressor
 from learners.sklearn_learner import sklearn_learner
 from scripts.consensus.consensus_wrapper import consensus_wrapper as consensus
 from helpers.dock import dock
 from helpers.helpers import initialize_logging, log_and_save, log_list, remove_right_df_from_left_df
-
+import sys
+from sklearn.model_selection import GridSearchCV
 import time
 import os
+from helpers.helpers import convert_list_of_smiles_to_morgan_fingerprints
 ##############ADJUST THESE########################
 BATCH_SIZE = 1000
 AL_CYCLES = 10
 TOPX = 5000
 CONSENSUS_METHOD = "WeightedSumModel_avg_scaled"
+parameter_grid ={
+    'n_estimators': [100, 200, 300, 400, 500],
+    'max_depth': [5, 10, 15, 20, 25, None],
+    'min_samples_split': [2, 5, 10, 15, 20],
+    'min_samples_leaf': [1, 2, 3, 4, 5],
+    'bootstrap': [True, False],
+    "warm_start": [True]
+}
+MODEL = RandomForestRegressor()
 #################################################
-#cs_methods = ['ECR_avg_scaled', 'ECR_best_scaled', 'RbR_avg_scaled', 'RbR_best_scaled', 'RbV_avg_scaled', 'RbV_best_scaled', 'Zscore_avg_scaled', 'Zscore_best_scaled', 'Pareto_rank_avg_scaled', 'Pareto_rank_best_scaled', 'TOPSIS_avg_scaled', 'TOPSIS_best_scaled', 'WeightedSumModel_avg_scaled', 'WeightedSumModel_best_scaled']
+cs_methods = ['ECR_avg_scaled', 'ECR_best_scaled', 'RbR_avg_scaled', 'RbR_best_scaled', 'RbV_avg_scaled', 'RbV_best_scaled', 'Zscore_avg_scaled', 'Zscore_best_scaled', 'Pareto_rank_avg_scaled', 'Pareto_rank_best_scaled', 'TOPSIS_avg_scaled', 'TOPSIS_best_scaled', 'WeightedSumModel_avg_scaled', 'WeightedSumModel_best_scaled']
 # Open log file for writing
 
 log_file = initialize_logging(__file__)
@@ -38,10 +51,11 @@ inital_random_sample = random_query_function(smids_final_input, None, BATCH_SIZE
 smids_final_input = remove_right_df_from_left_df(smids_final_input, inital_random_sample)
 docked_inital_random_sample  = dock(ground_truth_df_path, inital_random_sample)
 consens = consensus(docked_inital_random_sample, CONSENSUS_METHOD)
-
-
-
-learner = chemprop_learner(greedy_query_function, consens.loc[:,"SMILES"].values, consens.loc[:,"consensus"].values, batch_size=BATCH_SIZE)
+gridsearch= GridSearchCV(MODEL, parameter_grid, cv=5, scoring='neg_mean_absolute_error')
+gridsearch.fit(convert_list_of_smiles_to_morgan_fingerprints(consens.loc[:,"SMILES"].values),  consens.loc[:,"consensus"].values)
+best_model = gridsearch.best_estimator_
+log_and_save(f"Best model:{gridsearch.best_parmameters_}",log_file)
+learner = sklearn_learner(greedy_query_function, consens.loc[:,"SMILES"].values, consens.loc[:,"consensus"].values, model=best_model, batch_size=BATCH_SIZE)
 log_and_save(f"Batch size: {BATCH_SIZE}; active learning cycles: {AL_CYCLES}; top X of X percentage score: {TOPX}; Machine learning architecture:{learner.getName()}; Consensus Method:{CONSENSUS_METHOD};",log_file)
 topxlist = []
 topxlist.append(evaluate_learner(learner))
