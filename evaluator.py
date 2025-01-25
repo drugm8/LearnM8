1885a31904f97d4017f8a1ed86c25be85dbc8fe4
Max <maba00001@stud.uni-saarland.de>
2025-01-25 16:13:22 +0100

    substatial progress in evaluator

--- evaluator.py
+++ evaluator.py
@@ -5,6 +5,8 @@ import json
 import itertools
 import math
 import statistics
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 FOLDER_PATH = "./new/results/"
 TOPXPERCENTAGE_AS_FLOAT = 0.01
@@ -16,6 +18,19 @@ path_list= [
     './data/MAPK1_2ojg_scoring_and_consensus_maxAL.csv', 
     './data/PKM2_3gr4_scoring_and_consensus_maxAL.csv', 
     './data/VDR_3a2j_scoring_and_consensus_maxAL.csv']
+
+activity_file_mapping = {
+   './data/ALDH1_4x4l_scoring_and_consensus_maxAL.csv': './data/ALDH1_4x4l_activity_data.csv',
+   './data/FEN1_5fv7_scoring_and_consensus_maxAL.csv': './data/FEN1_5fv7_activity_data.csv',
+   './data/GBA_2v3e_scoring_and_consensus_maxAL.csv': './data/GBA_2v3e_activity_data.csv', 
+   './data/KAT2A_5h84_scoring_and_consensus_maxAL.csv': './data/KAT2A_5h84_activity_data.csv',
+   './data/MAPK1_2ojg_scoring_and_consensus_maxAL.csv': './data/MAPK1_2ojg_activity_data.csv',
+   './data/PKM2_3gr4_scoring_and_consensus_maxAL.csv': './data/PKM2_3gr4_activity_data.csv',
+   './data/VDR_3a2j_scoring_and_consensus_maxAL.csv': './data/VDR_3a2j_activity_data.csv'
+}
+
+
+
 
 _param_combinations_to_avg = {'learner': ['rf_learner'], 
                              'hyperparameter_tuning': [False], 
@@ -39,10 +54,56 @@ def coordinate_friendly_list_print(lis):
     else:
         for i in range(0,len(lis)):
             print("("+str(i)+","+str(lis[i])+")")
+
+def average_lists(list_of_lists):
+   if not list_of_lists:
+       return []
+   return [sum(col)/len(list_of_lists) for col in zip(*list_of_lists)]
     
 
-def enrichmentfactor(activity_path, cycle_protocol):
-    pass
+def enrichmentfactor(activity_path, cycle_protocol, threshold_percent):
+    threshold_percent_actual = threshold_percent/100
+
+    ef_list = []
+    threshold = math.floor(cycle_protocol.shape[0]*threshold_percent_actual)
+    activity = pd.read_csv(activity_path, dtype={'ID': str, 'Activity': int})
+    all_actives = sum(activity.iloc[:,-1:].values.astype(int))
+    all_compounds = activity.shape[0]
+
+    ef_list = []
+    for col in cycle_protocol.drop('ID', axis=1).columns:
+        # Sort once per column
+        top_ids = cycle_protocol.nlargest(threshold, col)['ID']
+
+        # Vectorized lookup using merge/join
+        active_count = activity[activity['ID'].isin(top_ids)]['Activity'].sum()
+
+        ef = (active_count/threshold) * (all_compounds/all_actives)
+        ef_list.append(ef)
+
+    
+    return ef_list
+
+
+def enrichmentfactor_GT(activity_path, ground, threshold_percent):
+    threshold_percent_actual = threshold_percent/100
+
+    threshold = math.floor(ground.shape[0]*threshold_percent_actual)
+    activity = pd.read_csv(activity_path, dtype={'ID': str, 'Activity': int})
+    all_actives = sum(activity.iloc[:,-1:].values.astype(int))
+    all_compounds = activity.shape[0]
+
+
+    # Sort once per column
+    last_col = ground.columns[-1]
+    top_ids = ground.nlargest(threshold, last_col)['ID']
+    # Vectorized lookup using merge/join
+    active_count = activity[activity['ID'].isin(top_ids)]['Activity'].sum()
+    ef = (active_count/threshold) * (all_compounds/all_actives)
+
+
+    
+    return ef
 
 def is_dict_in_list(target_dict, dict_list):
     #checks whether a dictionary is in a list of dictionaries
@@ -417,14 +478,106 @@ def extensiveGBA():
             print(single_experiment_with_wiskers(_avg_of_all_greedy))
 
 
+def avg_ef_per_combo(combo,threshold):
+    for hash in os.listdir(FOLDER_PATH):
+        combination_path = FOLDER_PATH+ hash + '/combination.txt'
+        if not os.path.exists(combination_path):
+            #print("no combination for hash", hash)
+            continue
 
 
 
+        combinations = {}
+        with open(FOLDER_PATH+ hash + '/combination.txt', 'r') as file:
+            combinations = json.loads(file.read())
 
+        #selection logic
+        if combo != combinations:
+            continue
+
+        data_path = combinations["ground_truth_path"]
+        activity_path = activity_file_mapping[data_path]
+        res = []
+        for stat in os.listdir(FOLDER_PATH+ hash):
+            if (stat == "combination.txt") | ( not os.path.exists(FOLDER_PATH+ hash + '/' + stat + "/cache.csv")):
+                continue
+            if stat == "7" or stat == "8" or stat == "9":
+                continue
+            
+            cycle_protocol = pd.read_csv(FOLDER_PATH+ hash + '/' + stat+"/cache.csv")
+
+
+            if (cycle_protocol.shape[1]==3 and combinations["cycles"]== -1) | (cycle_protocol.shape[1]==12 and combinations["cycles"]==10):
+                ef = enrichmentfactor(activity_path, cycle_protocol, threshold)
+                res.append(ef)
+            else:
+                print(hash + " ------ " + stat + " is invalid with the config:")
+                print(combinations)
+
+     
+    return average_lists(res)
+
+def single_ef_table_column(pathlistid):
+        combo = {'learner': 'rf_learner', 
+                                 'hyperparameter_tuning': False, 
+                                 'batch_size_percentage': 1, 
+                                 'smids_input_path': path_list[pathlistid], 
+                                 'ground_truth_path': path_list[pathlistid], 
+                                 'cycles': 10, #!ONLY ONE
+                                 'column_to_learn': '', 
+                                 'do_scoring_function_list_prediction': False, 
+                                 'first_query_function': 'random_query_function', 
+                                 'query_function': 'greedy_query_function'}
+
+        ef_activity_10 = avg_ef_per_combo(combo,10)
+        ef_activity_1 = avg_ef_per_combo(combo,1)
+        ef_activity_01 = avg_ef_per_combo(combo,0.1)
+
+        ef_gt_10 = enrichmentfactor_GT(activity_file_mapping[path_list[pathlistid]], pd.read_csv(path_list[pathlistid]), 10)
+        ef_gt_1 = enrichmentfactor_GT(activity_file_mapping[path_list[pathlistid]], pd.read_csv(path_list[pathlistid]), 1)
+        ef_gt_01 = enrichmentfactor_GT(activity_file_mapping[path_list[pathlistid]], pd.read_csv(path_list[pathlistid]), 0.1)
+        #print("start")
+
+        
+
+        print(    f"""\\begin{{tabular}}{{c|c|c}} 
+        \SI{{{ef_activity_10[-1]}}}{{\percent}} & 
+        \SI{{{ef_activity_1[-1]}}}{{\percent}} & 
+        \SI{{{ef_activity_01[-1]}}}{{\percent}} \end{{tabular}}  &
+        \\begin{{tabular}}{{c|c|c}} 
+        \SI{{{ef_gt_10[-1]}}}{{\percent}} &
+        \SI{{{ef_gt_1[-1]}}}{{\percent}} &
+        \SI{{{ef_gt_01[-1]}}}{{\percent}} \end{{tabular}} \\\\""")
+
+def eftable():
+
+
+
+    print("\\textbf{FEN1}&")
+    single_ef_table_column(0)
+
+    print("\\textbf{GBA}&")
+    single_ef_table_column(1)
+
+    print("\\textbf{KAT2A}&")
+    single_ef_table_column(2)
+
+    print("\\textbf{MAPK1}&")
+    single_ef_table_column(3)
+
+    print("\\textbf{PKM2}&")
+    single_ef_table_column(4)
+
+    print("\\textbf{VDR}&")
+    single_ef_table_column(5)
+        
+     
+ 
 
 def main():
     #cross_target_avg_rf_gredy_vs_random()
     #cross_target_batch_size_comparison_greedy()
     #tenvstwo()
-    extensiveGBA()
+    #extensiveGBA()
+    eftable()
 main()
