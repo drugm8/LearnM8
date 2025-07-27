8ebc066becbe6d425955c970dd05515f87aa13f1
Tonylac77 <alacournola@gmail.com>
2025-07-27 09:38:09 +0200

    Fix import statement for utility functions in __init__.py

--- learnm8/utils/__init__.py
+++ learnm8/utils/__init__.py
@@ -1,6 +1,6 @@
 # learnm8/utils/__init__.py
 """Utility functions for LearnM8."""
 
-from .chemistry import smiles_to_morgan_fingerprint, smiles_to_fingerprints
+from .featurizers import smiles_to_morgan_fingerprint, smiles_to_fingerprints
 
 __all__ = ['smiles_to_morgan_fingerprint', 'smiles_to_fingerprints']
