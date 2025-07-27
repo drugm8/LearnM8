52e3a41155249367c2681d8d24190cc85c50c719
Tonylac77 <alacournola@gmail.com>
2025-07-27 09:37:41 +0200

    Add missing learner imports in the __init__.py module

--- learnm8/learners/__init__.py
+++ learnm8/learners/__init__.py
@@ -3,3 +3,10 @@
 
 from .base import SklearnLearner
 from .random_forest import RandomForestLearner
+from .advanced_random_forest import AdvancedRandomForestLearner
+from .mlp import MLPLearner
+from .linear_regression import LinearRegressionLearner
+from .decision_tree import DecisionTreeLearner
+from .gradient_boosting import XGBoostLearner
+from .gaussian_pytorch import GaussianPyTorchLearner
+from .pytorch_mlp import PyTorchMLPLearner
