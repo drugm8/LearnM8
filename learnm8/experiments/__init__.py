167fbef1d3fe2e85226dde192d357016696dd40d
Tonylac77 <alacournola@gmail.com>
2025-07-31 09:56:30 +0200

    feat: Remove experiment runner and configuration files to simplify the repository

--- learnm8/experiments/__init__.py
+++
@@ -1,6 +0,0 @@
-# learnm8/experiments/__init__.py
-"""Experiment runner and configuration."""
-
-from .runner import create_experiment_config, run_experiment
-
-__all__ = ['create_experiment_config', 'run_experiment']
