93b296cf962dc045b80d82c95901bc6fceaeef7b
Tonylac77 <alacournola@gmail.com>
2025-10-30 17:49:15 +0100

    Add logging setup functions to module exports for improved accessibility

--- learnm8/utils/__init__.py
+++ learnm8/utils/__init__.py
@@ -3,11 +3,14 @@
 
 from .featurizers import smiles_to_morgan_fingerprint, smiles_to_fingerprints
 from .cycle_utils import parse_cycle_spec, summarize_cycle_spec, validate_cycle_spec
+from .logging import setup_logging, setup_logging_for_environment
 
 __all__ = [
-    'smiles_to_morgan_fingerprint', 
+    'smiles_to_morgan_fingerprint',
     'smiles_to_fingerprints',
     'parse_cycle_spec',
     'summarize_cycle_spec',
-    'validate_cycle_spec'
+    'validate_cycle_spec',
+    'setup_logging',
+    'setup_logging_for_environment'
 ]
