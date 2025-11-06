bc1b63f54a0526c630c67bed851a05efe8d1dca1
Tonylac77 <alacournola@gmail.com>
2025-11-06 11:07:57 +0100

    Add ChempropLearner for graph-based molecular predictions with optional descriptors

--- learnm8/learners/torch/__init__.py
+++ learnm8/learners/torch/__init__.py
@@ -4,8 +4,17 @@ from .mlp import MLPLearner
 from .mc_dropout import MCDropoutLearner
 from .fastprop_learner import FastpropLearner
 
+try:
+    from .chemprop_learner import ChempropLearner
+    _chemprop_available = True
+except ImportError:
+    _chemprop_available = False
+
 __all__ = [
     'MLPLearner',
     'MCDropoutLearner',
     'FastpropLearner'
 ]
+
+if _chemprop_available:
+    __all__.append('ChempropLearner')
