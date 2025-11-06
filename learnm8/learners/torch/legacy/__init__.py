581f3406f25650c065979f6436e6a72fc8e4abf8
Tonylac77 <alacournola@gmail.com>
2025-11-06 20:03:28 +0100

    Remove legacy PyTorch learners: GaussianPyTorchLearner and PyTorchMLPLearner

--- learnm8/learners/torch/legacy/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Legacy PyTorch-based learners."""
-
-from .gaussian_pytorch import GaussianPyTorchLearner
-from .pytorch_mlp import PyTorchMLPLearner
-
-__all__ = [
-    'GaussianPyTorchLearner',
-    'PyTorchMLPLearner'
-]
